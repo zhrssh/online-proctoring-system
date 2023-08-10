799cebd0ecfc5feff54e727eab7f65d19862faac
zhrssh <98146850+zhrssh@users.noreply.github.com>
2023-08-11 00:26:40 +0800

    Added webcam recording and new pages

--- src/webapp/src/pages/proctor_create_exam.jsx
+++ src/webapp/src/pages/proctor_create_exam.jsx
@@ -1,208 +1,222 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button'
-import Container from '@mui/material/Container';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid'
-import Paper from '@mui/material/Paper';
-import Radio from '@mui/material/Radio'
-import TextField from '@mui/material/TextField';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import { useNavigate } from 'react-router-dom';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Radio from "@mui/material/Radio";
+import TextField from "@mui/material/TextField";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { useNavigate } from "react-router-dom";
 
 export default function ProctorCreateExam() {
-    const [selectedValue, setSelectedValue] = React.useState('a');
-    const navigate = useNavigate();
+	const [selectedValue, setSelectedValue] = React.useState("a");
+	const navigate = useNavigate();
 
-    function handleReturn() {
-        navigate('/proctor')
-    }
+	function handleReturn() {
+		navigate("/proctor");
+	}
 
-    function setAnswer(event) {
-        setSelectedValue(event.target.value);
-    }
+	function setAnswer(event) {
+		setSelectedValue(event.target.value);
+	}
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            question: data.get('question'),
-            options: {
-                0: data.get('optionOne'),
-                1: data.get('optionTwo'),
-                2: data.get('optionThree'),
-                3: data.get('optionFour')
-            },
-            answer: selectedValue
-        });
-    }
+	function handleSubmit(event) {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
+		console.log({
+			question: data.get("question"),
+			options: {
+				0: data.get("optionOne"),
+				1: data.get("optionTwo"),
+				2: data.get("optionThree"),
+				3: data.get("optionFour"),
+			},
+			answer: selectedValue,
+		});
+	}
 
-    return (
-        <React.Fragment>
-            <CssBaseline />
-            <Box sx={{ flexGrow: 1 }}>
-                <AppBar position="static">
-                    <Toolbar>
-                        <Typography variant="h6" component="div">
-                            Create Exam
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-            </Box>
-            <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
-                <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-                    <Box component="div" sx={{textAlign: "justify"}}>
-                        <Typography component="h1" variant="h6">
-                            Question #1
-                        </Typography>
-                        <Typography component="p" variant="body2">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </Typography>
-                        <Grid container sx={{ marginTop:2, marginBottom: 2 }}>
-                            <Grid item xs={12}>
-                                <Typography component="p" variant="button">
-                                    Option A
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Typography component="p" variant="button">
-                                    Option B
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Typography component="p" variant="button">
-                                    Option C
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Typography component="p" variant="button">
-                                    Option D
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                        <Typography component="p" variant="caption">
-                            Correct Answer: B
-                        </Typography>
-                    </Box>
-                </Paper>
-            </Container>
-            <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
-                <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-                    <Box component="form" onSubmit={handleSubmit} noValidate>
-                        <TextField
-                            required
-                            id="question"
-                            label="Question"
-                            name="question"
-                            placeholder="Type your question here..."
-                            multiline
-                            rows={4}
-                            fullWidth
-                            variant="filled"
-                            sx={{
-                                marginBottom: 2
-                            }}
-                        />
-                        <Grid container sx={{marginBottom: 2}}>
-                            <Grid item xs={2} sx={{ alignSelf: 'end' }}>
-                                <Radio
-                                    checked={selectedValue === 'a'}
-                                    onChange={setAnswer}
-                                    value="a"
-                                    name="radio-buttons"
-                                    inputProps={{ 'aria-label': 'A' }}
-                                    size='small'
-                                />
-                            </Grid>
-                            <Grid item xs={10}>
-                                <TextField
-                                    id="optionOne"
-                                    name="optionOne"
-                                    label="Option 1"
-                                    fullWidth
-                                    variant="standard"
-                                />
-                            </Grid>
-                            <Grid item xs={2} sx={{ alignSelf: 'end' }}>
-                                <Radio
-                                    checked={selectedValue === 'b'}
-                                    onChange={setAnswer}
-                                    value="b"
-                                    name="radio-buttons"
-                                    inputProps={{ 'aria-label': 'B' }}
-                                    size='small'
-                                />
-                            </Grid>
-                            <Grid item xs={10}>
-                                <TextField
-                                    id="optionTwo"
-                                    name="optionTwo"
-                                    label="Option 2"
-                                    fullWidth
-                                    variant="standard"
-                                />
-                            </Grid>
-                            <Grid item xs={2} sx={{ alignSelf: 'end' }}>
-                                <Radio
-                                    checked={selectedValue === 'c'}
-                                    onChange={setAnswer}
-                                    value="c"
-                                    name="radio-buttons"
-                                    inputProps={{ 'aria-label': 'C' }}
-                                    size='small'
-                                />
-                            </Grid>
-                            <Grid item xs={10}>
-                                <TextField
-                                    id="optionThree"
-                                    name="optionThree"
-                                    label="Option 3"
-                                    fullWidth
-                                    variant="standard"
-                                />
-                            </Grid>
-                            <Grid item xs={2} sx={{ alignSelf: 'end' }}>
-                                <Radio
-                                    checked={selectedValue === 'd'}
-                                    onChange={setAnswer}
-                                    value="d"
-                                    name="radio-buttons"
-                                    inputProps={{ 'aria-label': 'D' }}
-                                    size='small'
-                                />
-                            </Grid>
-                            <Grid item xs={10}>
-                                <TextField
-                                    id="optionFour"
-                                    name="optionFour"
-                                    label="Option 4"
-                                    fullWidth
-                                    variant="standard"
-                                />
-                            </Grid>
-                        </Grid>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 2, mb: 2 }}
-                        >
-                            Create Question
-                        </Button>
-                    </Box>
-                    <Button
-                        type="button"
-                        fullWidth
-                        onClick={handleReturn}
-                        variant="contained"
-                        sx={{ mb: 2 }}
-                    >
-                        Done
-                    </Button>
-                </Paper>
-            </Container>
-        </React.Fragment>
-    );
+	return (
+		<React.Fragment>
+			<CssBaseline />
+			<Box sx={{ flexGrow: 1 }}>
+				<AppBar position="static">
+					<Toolbar>
+						<Typography variant="h6" component="div">
+							Create Exam
+						</Typography>
+					</Toolbar>
+				</AppBar>
+			</Box>
+			<Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+				<Paper
+					variant="outlined"
+					sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+					<Box component="div" sx={{ textAlign: "justify" }}>
+						<Typography component="h1" variant="h6">
+							Question #1
+						</Typography>
+						<Typography component="p" variant="body2">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							enim ad minim veniam, quis nostrud exercitation ullamco laboris
+							nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+							reprehenderit in voluptate velit esse cillum dolore eu fugiat
+							nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+							sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</Typography>
+						<Grid container sx={{ marginTop: 2, marginBottom: 2 }}>
+							<Grid item xs={12}>
+								<Typography component="p" variant="button">
+									Option A
+								</Typography>
+							</Grid>
+							<Grid item xs={12}>
+								<Typography component="p" variant="button">
+									Option B
+								</Typography>
+							</Grid>
+							<Grid item xs={12}>
+								<Typography component="p" variant="button">
+									Option C
+								</Typography>
+							</Grid>
+							<Grid item xs={12}>
+								<Typography component="p" variant="button">
+									Option D
+								</Typography>
+							</Grid>
+						</Grid>
+						<Typography component="p" variant="caption">
+							Correct Answer: B
+						</Typography>
+					</Box>
+				</Paper>
+			</Container>
+			<Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+				<Paper
+					variant="outlined"
+					sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+					<Box component="form" onSubmit={handleSubmit} noValidate>
+						<Typography
+							component="h6"
+							variant="h6"
+							sx={{ textAlign: "start", marginBottom: "0.5em" }}>
+							Create your question here
+						</Typography>
+						<TextField
+							required
+							id="question"
+							label="Question"
+							name="question"
+							placeholder="Type your question here..."
+							multiline
+							rows={4}
+							fullWidth
+							variant="filled"
+							sx={{
+								marginBottom: 2,
+							}}
+						/>
+						<Grid container sx={{ marginBottom: 2 }}>
+							<Grid item xs={2} sx={{ alignSelf: "end" }}>
+								<Radio
+									checked={selectedValue === "a"}
+									onChange={setAnswer}
+									value="a"
+									name="radio-buttons"
+									inputProps={{ "aria-label": "A" }}
+									size="small"
+								/>
+							</Grid>
+							<Grid item xs={10}>
+								<TextField
+									id="optionOne"
+									name="optionOne"
+									label="Option 1"
+									fullWidth
+									variant="standard"
+								/>
+							</Grid>
+							<Grid item xs={2} sx={{ alignSelf: "end" }}>
+								<Radio
+									checked={selectedValue === "b"}
+									onChange={setAnswer}
+									value="b"
+									name="radio-buttons"
+									inputProps={{ "aria-label": "B" }}
+									size="small"
+								/>
+							</Grid>
+							<Grid item xs={10}>
+								<TextField
+									id="optionTwo"
+									name="optionTwo"
+									label="Option 2"
+									fullWidth
+									variant="standard"
+								/>
+							</Grid>
+							<Grid item xs={2} sx={{ alignSelf: "end" }}>
+								<Radio
+									checked={selectedValue === "c"}
+									onChange={setAnswer}
+									value="c"
+									name="radio-buttons"
+									inputProps={{ "aria-label": "C" }}
+									size="small"
+								/>
+							</Grid>
+							<Grid item xs={10}>
+								<TextField
+									id="optionThree"
+									name="optionThree"
+									label="Option 3"
+									fullWidth
+									variant="standard"
+								/>
+							</Grid>
+							<Grid item xs={2} sx={{ alignSelf: "end" }}>
+								<Radio
+									checked={selectedValue === "d"}
+									onChange={setAnswer}
+									value="d"
+									name="radio-buttons"
+									inputProps={{ "aria-label": "D" }}
+									size="small"
+								/>
+							</Grid>
+							<Grid item xs={10}>
+								<TextField
+									id="optionFour"
+									name="optionFour"
+									label="Option 4"
+									fullWidth
+									variant="standard"
+								/>
+							</Grid>
+						</Grid>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							sx={{ mt: 2, mb: 2 }}>
+							Create Question
+						</Button>
+					</Box>
+					<Button
+						type="button"
+						fullWidth
+						onClick={handleReturn}
+						variant="contained"
+						sx={{ mb: 2 }}>
+						Done
+					</Button>
+				</Paper>
+			</Container>
+		</React.Fragment>
+	);
 }
---
+++ src/webapp/src/pages/proctor_create_exam_success.jsx
@@ -0,0 +1,74 @@
+import Alert from "@mui/material/Alert";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import CenterFocusWeakOutlinedIcon from "@mui/icons-material/CenterFocusWeakOutlined";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const defaultTheme = createTheme();
+
+export default function ProctorCreateExamSuccess() {
+	const navigate = useNavigate();
+	const [success, setSuccess] = useState(true);
+
+	// Returns to the main proctor page
+	function handleReturn() {
+		navigate("/proctor");
+	}
+
+	// Checks if exam has successfully created
+	function handleSuccess(value) {
+		setSuccess(value);
+	}
+
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Box
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}>
+					<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+						<CenterFocusWeakOutlinedIcon />
+					</Avatar>
+					<Typography component="h1" variant="h6">
+						Online Proctoring System DEMO
+					</Typography>
+					{success ? (
+						<Box component="div" marginY="0.5em">
+							<Alert severity="success">
+								Exam successfully created! Your exam code is written below.
+							</Alert>
+							<Typography
+								component="h3"
+								variant="h3"
+								sx={{ marginTop: "0.5em", marginBottom: "0.5em" }}>
+								abcd123
+							</Typography>
+						</Box>
+					) : (
+						<Alert severity="error">
+							An error has occurred when creating the exam. Please try again.
+						</Alert>
+					)}
+
+					<Button
+						type="button"
+						fullWidth
+						variant="contained"
+						onClick={handleReturn}>
+						Done
+					</Button>
+				</Box>
+			</Container>
+		</ThemeProvider>
+	);
+}
