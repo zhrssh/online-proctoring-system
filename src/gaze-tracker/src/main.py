c89bb0e6ab64f1414e6830fa79a71de5e8ea8300
zhrssh <98146850+zhrssh@users.noreply.github.com>
2023-08-19 18:56:11 +0800

    gaze tracker added

---
+++ src/gaze-tracker/src/main.py
@@ -0,0 +1,7 @@
+import head_pose as hp
+
+
+FILE_PATH = r"C:\Users\zheri\Documents\dataset\cheating or not cheating\not cheating\2023-08-16_23-44-06.mp4"
+
+if __name__ == '__main__':
+    hp.captureVideo(FILE_PATH)
